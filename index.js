c1dc8f0afa560709f930185d46b22aab2684aa62
Edivansanto5 <edivansantos1478@gmail.com>
2023-05-25 14:34:34 -0300

    Commit 5 - Separando Apresentação dos Cálculos

--- index.js
+++ index.js
@@ -14,50 +14,48 @@ function gerarFaturaStr (fatura, pecas) {
     function getPeca(apresentacao) {
       return pecas[apresentacao.id];
     }
+
+    function calcularTotalApresentacao(apre){
+      let total = 0;
   
-    for (let apre of fatura.apresentacoes) {
-
-      function calcularTotalApresentacao(apre){
-        let total = 0;
-    
-        switch (getPeca(apre).tipo) {
-        case "tragedia":
-          total = 40000;
-          if (apre.audiencia > 30) {
-            total += 1000 * (apre.audiencia - 30);
-          }
-          break;
-        case "comedia":
-          total = 30000;
-          if (apre.audiencia > 20) {
-            total += 10000 + 500 * (apre.audiencia - 20);
-          }
-          total += 300 * apre.audiencia;
-          break;
-        default:
-            throw new Error(`Peça desconhecia: ${getPeca(apre).tipo}`);
+      switch (getPeca(apre).tipo) {
+      case "tragedia":
+        total = 40000;
+        if (apre.audiencia > 30) {
+          total += 1000 * (apre.audiencia - 30);
         }
-    
-        // créditos para próximas contratações
-        function calcularCredito(apre) {
-          let creditos = 0;
-          creditos += Math.max(apre.audiencia - 30, 0);
-          if (getPeca(apre).tipo === "comedia") 
-             creditos += Math.floor(apre.audiencia / 5);
-          return creditos;   
+        break;
+      case "comedia":
+        total = 30000;
+        if (apre.audiencia > 20) {
+          total += 10000 + 500 * (apre.audiencia - 20);
         }
-        
-        creditos += calcularCredito(apre);
-    
-        // mais uma linha da fatura
-        faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos)\n`;
-        totalFatura += total;
-
-        return total;
+        total += 300 * apre.audiencia;
+        break;
+      default:
+          throw new Error(`Peça desconhecia: ${getPeca(apre).tipo}`);
       }
+  
+      // créditos para próximas contratações
+      function calcularCredito(apre) {
+        let creditos = 0;
+        creditos += Math.max(apre.audiencia - 30, 0);
+        if (getPeca(apre).tipo === "comedia") 
+           creditos += Math.floor(apre.audiencia / 5);
+        return creditos;   
+      }
+      
+      creditos += calcularCredito(apre);
+  
+      // mais uma linha da fatura
+      faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos)\n`;
+      totalFatura += total;
 
-      let total = calcularTotalApresentacao(apre);
-
+      return total;
+    }
+  
+    for (let apre of fatura.apresentacoes){
+      faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
     }
     faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
     faturaStr += `Créditos acumulados: ${creditos} \n`;
