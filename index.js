e00dd8244e9034f263b490b9712622ad803c7c6b
Edivansanto5 <edivansantos1478@gmail.com>
2023-05-25 14:40:21 -0300

    Commit 5 - Separando Apresentação dos Cálculos

--- index.js
+++ index.js
@@ -53,13 +53,21 @@ function gerarFaturaStr (fatura, pecas) {
 
       return total;
     }
+
+    function calcularTotalFatura() {
+      return totalFatura;
+    }
+
+    function calcularTotalCreditos() {
+      return creditos;
+    }
   
     for (let apre of fatura.apresentacoes){
       faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
     }
-    faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
-    faturaStr += `Créditos acumulados: ${creditos} \n`;
-    return faturaStr;
+    faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura())}\n`;
+    faturaStr += `Créditos acumulados: ${calcularTotalCreditos()} \n`;
+  return faturaStr;
   }
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
